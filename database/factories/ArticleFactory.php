468f09e837df995874106c772dc6b5843a4c9ee8
philippe damen <philippe@thinktomorrow.be>
2017-09-05 16:52:11 +0200

    change last occurence of old vendor name

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
-use Yinx\AssetLibrary\Test\stubs\Article;
+use Thinktomorrow\AssetLibrary\Test\stubs\Article;
 
 /*
 |--------------------------------------------------------------------------
